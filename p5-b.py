b46dd6e6205d2a25e3c71a2e72538b81dc899057
ankitsejwal <ankit@sejw.al>
2018-01-25 00:27:26 +1100

    replace print with return

--- p5-b.py
+++ p5-b.py
@@ -7,14 +7,15 @@
 def modify_string(word):
     first_char = word[0]        # set first character of our string to first_char variable
     new_string = first_char     # add first_char to our new empty string, we will add more later
+
     for pointer in word[1:]:
         if pointer == first_char:
             new_string += '$'
         else:
             new_string += pointer
 
-    print(new_string)
+    return new_string
 
 if __name__ == '__main__':
-    modify_string('elephant')
-    modify_string('arastratiosphecomyia')
+    print(modify_string('elephant'))
+    print(modify_string('arastratiosphecomyia'))
