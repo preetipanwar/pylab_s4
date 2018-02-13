02b13dd2aaaa8f85cf248ed7c83838d83551e674
ankitsejwal <ankit@sejw.al>
2018-02-13 15:55:27 +1100

    code refactor

--- p2-alternative.py
+++ p2-alternative.py
@@ -9,15 +9,13 @@
 lower_limit = int(input("Enter the lower limit: "))
 upper_limit = int(input("Enter the upper limit: "))
 
-
 for num in range(lower_limit, upper_limit + 1):
     if num > 1:
-        num_is_prime = True        # assuming the number is prime number
-        for i in range(2, num):     # looping between 2 and num
-            if (num % i) == 0:      # if number (i) completely divides num, then num is not a prime no.
-                num_is_prime = False
-                break               # if any no (i) can divide num, it means it isn't prime
+        num_is_prime = True             # assuming the number is prime number
+        for i in range(2, num):         # looping between 2 and num
+            if (num % i) == 0:          # if number (i) completely divides num, then num is not a prime
+                num_is_prime = False    # if any no (i) can divide num, it means it isn't prime
+                break                   # break the inner loop
 
-    
     if num_is_prime == True:
         print(num)
